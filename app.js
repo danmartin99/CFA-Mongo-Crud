c18b5c74c529e925c70c264c49992ea2a4dca857
Dan Martin <dan.martin99@me.com>
2017-05-24 11:03:58 +1000

    CORS

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var cors = require('cors');
 const authApi = require('./middleware/authApi');
 
 var app = express();
@@ -39,6 +40,7 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
